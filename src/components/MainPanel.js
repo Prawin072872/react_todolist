90e49c5cbd6c510221a32aac96023a3d2d2a8ead
prawin <prawin.j07@gmail.com>
2023-12-18 05:31:27 -0500

    Updated Reducers Actions Store

---
+++ src/components/MainPanel.js
@@ -0,0 +1,16 @@
+import React from "react";
+import MyButton from "./MyButton";
+import DivPanel from "./DivPanel";
+
+
+const MainPanel = () => {
+    return(
+        <div>
+            This is the Main Panel <MyButton></MyButton>
+            <DivPanel></DivPanel>
+        </div>
+    )
+
+}
+
+export default MainPanel
